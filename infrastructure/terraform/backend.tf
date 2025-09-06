65fd8d67dff435f3567c3a3b285c73861cccba38
jamiekariuki <jamiekariuki18@gmail.com>
2025-09-06 11:28:29 +0300

    fix: fixed remote backend for workspace

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,7 +1,17 @@
-terraform {
+/* terraform {
   backend "s3" {
     bucket = "850502433430-app-dev"
     region = "us-east-1"
     key    = "app.tfstate"
+  }
+} */
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-states"
+    key            = "networking/${terraform.workspace}/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
