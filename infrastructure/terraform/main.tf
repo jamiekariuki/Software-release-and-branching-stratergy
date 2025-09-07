b2333d2fd42538d84631ebd55d7ce26d9534de74
jamiekariuki <jamiekariuki18@gmail.com>
2025-09-07 17:48:59 +0300

    feat: added aws cli in ec2

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -76,6 +76,15 @@ module "ec2_instance" {
             apt-get update -y
             apt-get upgrade -y
 
+            sudo apt-get update -y
+            sudo apt-get install -y unzip
+
+            curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+            unzip awscliv2.zip
+            cd aws
+            sudo ./install
+            aws --version
+
             apt-get install -y docker.io
             systemctl enable docker
             systemctl start docker
