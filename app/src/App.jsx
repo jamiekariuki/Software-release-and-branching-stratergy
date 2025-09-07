ce2b6c626c2860a14327ef8f47eb965ac9acddbf
jamiekariuki <jamiekariuki18@gmail.com>
2025-09-07 19:06:14 +0300

    feat: added new terrafrom workflow

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,18 +1,12 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import "./App.scss";
 import Appv1 from "./components/appv1/appv1";
 import Appv2 from "./components/appv2/appv2";
 
 function App() {
-  const [version, setVersion] = useState("v1.3.9");
-
-  useEffect(() => {
-    setVersion(window._env_?.VERSION || "v0.0.0");
-  }, []);
-
   return (
     <div className="appN">
-      <h1>{version}</h1>
+      <h1>OUR APP</h1>
       <Appv1 />
       {/*  <Appv2/> */}
     </div>
