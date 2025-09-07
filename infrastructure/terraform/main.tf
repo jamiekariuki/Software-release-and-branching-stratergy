b11d75bf77345d712f8fc355188c2c8d8c3ee705
jamiekariuki <jamiekariuki18@gmail.com>
2025-09-07 16:11:44 +0300

    feat: added docker compose and docker in ec2 instance

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -71,6 +71,21 @@ module "ec2_instance" {
   
   create_security_group = false
 
+  user_data = <<-EOF
+            #!/bin/bash
+            apt-get update -y
+            apt-get upgrade -y
+
+            apt-get install -y docker.io
+            systemctl enable docker
+            systemctl start docker
+            usermod -aG docker ubuntu
+
+            curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            chmod +x /usr/local/bin/docker-compose
+            ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+          EOF
+
   tags = {
     Terraform   = "true"
     Environment = var.ENV_PREFIX
