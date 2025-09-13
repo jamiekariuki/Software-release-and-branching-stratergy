23814f081e03af00bf8e479e910dd974d7c5ae41
jamiekariuki <jamiekariuki18@gmail.com>
2025-09-13 23:55:05 +0300

    feat: added second circle

--- app/src/components/appv1/appv1.jsx
+++ app/src/components/appv1/appv1.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import "./appv1.scss"
+import React from "react";
+import "./appv1.scss";
 
 const Appv1 = () => {
   return (
-    <div className='app'>
-
-        <div className="container">
-            <div className="circles c1"/>
-            <div className="circles c2"/> 
-            <div className="circles c3"/>    
-        </div>
-      
+    <div className="app">
+      <div className="container">
+        <div className="circles c1" />
+        <div className="circles c2" />
+        <div className="circles c3" />
+        <div className="circles c3" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Appv1
+export default Appv1;
